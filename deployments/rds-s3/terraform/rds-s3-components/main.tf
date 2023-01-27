1678961da5ede7c3540fe3838204a5aad1647751
Gavin Goodrich <gavin.goodrich@ni.com>
2023-01-27 11:31:31 -0600

    just config

--- deployments/rds-s3/terraform/rds-s3-components/main.tf
+++ deployments/rds-s3/terraform/rds-s3-components/main.tf
@@ -136,9 +136,7 @@ module "kubeflow_dex" {
   }
   addon_context = var.addon_context
   depends_on = [module.kubeflow_istio]
-  set_values = {
-    "config" = var.dex_config
-  }
+  config = var.dex_config
 }
 
 module "kubeflow_oidc_authservice" {
