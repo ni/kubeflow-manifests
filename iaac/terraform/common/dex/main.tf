e9b65d9d6021b75be3a0266876830eb6773c7c25
Gavin Goodrich <gavin.goodrich@ni.com>
2023-01-27 14:29:49 -0600

    try yaml as values

--- iaac/terraform/common/dex/main.tf
+++ iaac/terraform/common/dex/main.tf
@@ -2,10 +2,5 @@ module "helm_addon" {
   source            = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.12.1"
   helm_config       = local.helm_config
   addon_context     = var.addon_context
-  set_values        = [ 
-    {
-      name = "config"
-      value = var.config
-    }
-  ]
+  values            = yamlencode(var.config)
 }
